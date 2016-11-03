b944ad66cdd0b9fba976353df6abacbdbe54d9f1
imsunhao <sunhao__159@163.com>
2016-11-03 17:15:42 +0800

    empty

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass/import-once/activate'
+require 'compass'
+require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style =  :compact
+# relative_assets = true
+line_comments = false
+# cache = false
+sourcemap = true
